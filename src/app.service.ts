0ca46887cef9b21a8feeade92eff4148c9f4fe04
mgandore <mihai15.catalin@yahoo.com>
2023-06-20 22:23:53 +0300

    [WIP] standardize the input image to a 512x512 dim

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as regression from 'regression';
 import * as sharp from "sharp";
-import { BUFFER_SAMPLE } from "./buffer.sample";
 import * as path from "node:path"
 
 export interface ProcessingResponse {
@@ -13,9 +12,10 @@ export interface ProcessingResponse {
 @Injectable()
 export class ImageProcessingService {
 
-	private readonly NEIGHBORHOOD_SIZE = 81; // 81 will be 3^4
-	private readonly SCALING_FACTOR = 3; // of the boxes
-	private readonly MIN_BOX_SIZE = 1
+	private readonly STANDARDIZED_IMAGE_SIZE = 512
+	private readonly NEIGHBORHOOD_SIZE = 128;
+	private readonly SCALING_FACTOR = 2;
+	private readonly MIN_BOX_SIZE = 2
 
 	public async uploadFile(file: Express.Multer.File): Promise<ProcessingResponse> {
 		const { data, info } = await sharp(`${file.destination}/${file.filename}`).greyscale().raw()
@@ -24,18 +24,54 @@ export class ImageProcessingService {
 		const grayscalePixelArray = new Uint8ClampedArray(data.buffer)
 		/**
 		 * Each value at [i][j] represents the intensity of the gray color between 0 and 255
-		 * This may be the only information needed to create the 3d plot on the UI sie 
+		 * This may be the only information needed to create the 3d plot on the UI side 
 		 */
 		const grayscaleImageMatrix: number[][] = this.toPixelMatrix(grayscalePixelArray, info.width);
-		const fractalDimensionMatrix: number[][] = this.getFractalDimensionMatrix(grayscaleImageMatrix);
+		const squaredImagePixels = this.squareMatrix(grayscaleImageMatrix)
+		const fractalDimensionMatrix: number[][] = this.getFractalDimensionMatrix(squaredImagePixels);
 		// console.log("FD matrix", fractalDimensionMatrix)
 		const heatmapImageSouceName: string = path.basename(await this.generateHeatmap(fractalDimensionMatrix))
 
-		return <ProcessingResponse>{ heatmapImageSourceName: heatmapImageSouceName, grayscaleData: grayscaleImageMatrix }
+		return <ProcessingResponse>{ heatmapImageSourceName: heatmapImageSouceName, grayscaleData: squaredImagePixels }
 	}
 
 	////////////////////////////////////////////**		 PRIVATE ZONE 		**/////////////////////////////////////////////////////
 
+	/**
+	 * 
+	 * @param pixelMatrix 
+	 * @returns a 512x512 matrix
+	 */
+	private squareMatrix(pixelMatrix: number[][]): number[][] {
+		let matrixClone: number[][] = pixelMatrix.map(row => [...row])
+		//HEIGHT
+		if (pixelMatrix.length > this.STANDARDIZED_IMAGE_SIZE) {
+			matrixClone = pixelMatrix.slice(0, this.STANDARDIZED_IMAGE_SIZE)
+		} else {
+			matrixClone = this.fillTheRows(pixelMatrix)
+		}
+		//WIDTH
+		if (pixelMatrix[0].length > this.STANDARDIZED_IMAGE_SIZE) {
+			matrixClone = matrixClone.map(row => row.slice(0, this.STANDARDIZED_IMAGE_SIZE))
+		} else {
+			let rowLength = pixelMatrix[0].length
+			let missingRowLength = this.STANDARDIZED_IMAGE_SIZE - rowLength
+			matrixClone = matrixClone.map(row => row.concat(new Array(missingRowLength).fill(0)))
+		}
+		return matrixClone
+	}
+
+	private fillTheRows(pixelMatrix: number[][]): number[][] {
+		const rowLength: number = pixelMatrix[0].length
+		const currentRowsCount: number = pixelMatrix.length
+		let matrixClone: number[][] = pixelMatrix.map(row => [...row])
+		let neededRowsCount: number = this.STANDARDIZED_IMAGE_SIZE - currentRowsCount
+		while (neededRowsCount > 0) {
+			matrixClone.push(new Array(rowLength).fill(0))
+			neededRowsCount--;
+		}
+		return matrixClone
+	}
 
 	private toPixelMatrix(pixelArray: Uint8ClampedArray, width: number): number[][] {
 		let matrix: number[][] = [];
@@ -59,44 +95,18 @@ export class ImageProcessingService {
 	}
 
 	private async generateHeatmap(matrix: number[][]): Promise<string> {
-		const height = matrix.length;
-		const width = matrix[0].length;
-		const image = Buffer.alloc(width * height * 3); // 3 bytes per pixel (RGB)
-
-		let i = 0;
-		for (let row = 0; row < height; row++) {
-			for (let col = 0; col < width; col++) {
-				const value = matrix[row][col];
-				const color = this.getColor(value);
-
-				image[i++] = color[0]; // Red
-				image[i++] = color[1]; // Green
-				image[i++] = color[2]; // Blue
-			}
-		}
-		const outputFilePath: string = `../box-counting-app-ui/src/assets/img-${Date.now().toString()}.png`
-		await sharp(image, { raw: { width, height, channels: 3 } })
-			.toFormat('png')
-			.toFile(outputFilePath)
-			.catch(() => { throw new Error("Heatmap creation failed") })
-		return outputFilePath;
+		return
 	}
 
-	private getColor(value: number): [number, number, number] {
-		const red = Math.round(255 * value);
-		const blue = Math.round(255 * (1 - value));
-		const green = 0;
-		return [red, green, blue];
-	}
+
 
 	private calculateFractalDimension(imageMatrix: number[][], centerX: number, centerY: number): number {
 		const data: regression.DataPoint[] = [];
 		const maxBoxSize: number = this.NEIGHBORHOOD_SIZE / 2;
 		let neighborhood: number[][] = this.getNeighborhood(imageMatrix, centerX, centerY, this.NEIGHBORHOOD_SIZE);
-		// console.log("Neighborhood", neighborhood)
-		neighborhood = this.binarizeWithOtsu(neighborhood)
+		// console.log("Neighborhood size", `${neighborhood.length}x${neighborhood[0].length}`)
 
-		for (let boxSize = this.MIN_BOX_SIZE; boxSize <= maxBoxSize; boxSize *= this.SCALING_FACTOR) {
+		for (let boxSize = this.MIN_BOX_SIZE; boxSize < maxBoxSize; boxSize *= this.SCALING_FACTOR) {
 			let boxesCount: number = 0;
 			// console.log(`BOX SIZE: ${boxSize}`)
 			for (let i = 0; i < this.NEIGHBORHOOD_SIZE; i += boxSize) {
@@ -117,68 +127,9 @@ export class ImageProcessingService {
 		// return 0;
 		const result = regression.linear(data);
 		const fractalDimension = result.equation[0];
-		return fractalDimension;
+		return -fractalDimension;
 	}
 
-	private binarizeWithOtsu(matrix: number[][]): number[][] {
-		// Calculate histogram
-		const histogram = new Array(256).fill(0);
-		const totalPixels = matrix.length * matrix[0].length;
-
-		for (let i = 0; i < matrix.length; i++) {
-			for (let j = 0; j < matrix[0].length; j++) {
-				const pixelValue = matrix[i][j];
-				histogram[pixelValue]++;
-			}
-		}
-
-		// Normalize histogram
-		const normalizedHistogram = histogram.map(count => count / totalPixels);
-
-		// Calculate cumulative sums
-		let sum = 0;
-		const cumulativeSum = normalizedHistogram.map((value) => {
-			sum += value;
-			return sum;
-		});
-
-		// Calculate between-class variance
-		let maxVariance = 0;
-		let threshold = 0;
-
-		for (let t = 0; t < 256; t++) {
-			const backgroundWeight = cumulativeSum[t];
-			const foregroundWeight = 1 - backgroundWeight;
-
-			const backgroundMean = cumulativeSum
-				.slice(0, t + 1)
-				.reduce((sum, value, index) => sum + index * normalizedHistogram[index], 0) / backgroundWeight;
-
-			const foregroundMean = cumulativeSum
-				.slice(t + 1)
-				.reduce((sum, value, index) => sum + index * normalizedHistogram[index], 0) / foregroundWeight;
-
-			const variance = backgroundWeight * foregroundWeight * Math.pow(backgroundMean - foregroundMean, 2);
-
-			if (variance > maxVariance) {
-				maxVariance = variance;
-				threshold = t;
-			}
-		}
-
-		// Binarize the matrix based on the threshold
-		const binarizedMatrix = [];
-
-		for (let i = 0; i < matrix.length; i++) {
-			const row = [];
-			for (let j = 0; j < matrix[0].length; j++) {
-				const pixelValue = matrix[i][j];
-				row.push(pixelValue > threshold ? 255 : 0);
-			}
-			binarizedMatrix.push(row);
-		}
-		return binarizedMatrix
-	}
 
 	private isBoxCountable(boxPixels: number[], mainPixel: number): boolean {
 		return boxPixels.every(pixel => pixel !== 0)
@@ -192,9 +143,9 @@ export class ImageProcessingService {
 	private getNeighborhood(imageMatrix: number[][], x: number, y: number, neighborhoodSize: number): number[][] {
 		const neighborhood = [];
 		let halfSize = Math.floor(neighborhoodSize / 2);
-		for (let i = x - halfSize; i <= x + halfSize; i++) {
+		for (let i = x - halfSize; i <= x + halfSize - 1; i++) {
 			const row = [];
-			for (let j = y - halfSize; j <= y + halfSize; j++) {
+			for (let j = y - halfSize; j <= y + halfSize - 1; j++) {
 				if (i < 0 || j < 0 || i >= imageMatrix.length || j >= imageMatrix[0].length) {
 					row.push(0)
 				} else {
